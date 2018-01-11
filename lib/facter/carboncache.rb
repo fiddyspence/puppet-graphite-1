dce824aa2e7b0fef29159482785e0717952a37ea
Chris Spence <chris@spence.org.uk>
2018-01-11 16:08:12 +0000

    Edit fact

--- lib/facter/carboncache.rb
+++ lib/facter/carboncache.rb
@@ -4,23 +4,30 @@ require 'json'
 Facter.add('graphitecache') do
   setcode do
     uri = URI.parse("http://localhost:8500/v1/catalog/service/carboncache")
-    JSON::parse(Net::HTTP.get_response(uri).body)
+    begin
+      JSON::parse(Net::HTTP.get_response(uri).body)
+    rescue
+      [ ]
+    end
   end
 end
 Facter.add('graphitecachehosts') do
   setcode do
+    return [] if Facter.value(:graphitecache).empty?
     cachenodes = Facter.value(:graphitecache)
     cachenodes.map { |node| node['Node'] }
   end
 end
 Facter.add('graphitecacheaddresses') do
   setcode do
+    return [] if Facter.value(:graphitecache).empty?
     cachenodes = Facter.value(:graphitecache)
     cachenodes.map { |node| { node['Node'] => node['Address'] }}
   end
 end
 Facter.add('graphitecachedestinations') do
   setcode do
+    return [] if Facter.value(:graphitecache).empty?
     cachenodes = Facter.value(:graphitecache)
     cachenodes.map { |node| "#{node['Address']}:#{node['ServicePort']}" }
   end
